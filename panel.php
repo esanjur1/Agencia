54a24914679210ad043d75387199f2e603b73f83
PauRuizJordano <cf.pau.ruiz@torreroja.cat>
2025-03-17 20:54:38 +0100

    He corregido el código

--- panel.php
+++ panel.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Administración Básico</title>
-    <link href="panel1.css" rel="stylesheet">
+    <link href="panel.css" rel="stylesheet">
 </head>
 <body>
 
